e40cf20cb49515999c7726647fe29d33a7b5fa24
Faris Ahmad <faris.s.ahmad@gmail.com>
2020-03-23 20:15:54 -0300

    ADD - Entidades Pedido,Pagamento e EstadoPagamento

--- src/main/java/com/udemy/spring/api/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/spring/api/cursomc/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.udemy.spring.api.cursomc;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,20 @@ import com.udemy.spring.api.cursomc.domain.Cidade;
 import com.udemy.spring.api.cursomc.domain.Cliente;
 import com.udemy.spring.api.cursomc.domain.Endereco;
 import com.udemy.spring.api.cursomc.domain.Estado;
+import com.udemy.spring.api.cursomc.domain.Pagamento;
+import com.udemy.spring.api.cursomc.domain.PagamentoComBoleto;
+import com.udemy.spring.api.cursomc.domain.PagamentoComCartao;
+import com.udemy.spring.api.cursomc.domain.Pedido;
 import com.udemy.spring.api.cursomc.domain.Produto;
+import com.udemy.spring.api.cursomc.domain.enums.EstadoPagamento;
 import com.udemy.spring.api.cursomc.domain.enums.TipoCliente;
 import com.udemy.spring.api.cursomc.repositories.CategoriaRepository;
 import com.udemy.spring.api.cursomc.repositories.CidadeRepository;
 import com.udemy.spring.api.cursomc.repositories.ClienteRepository;
 import com.udemy.spring.api.cursomc.repositories.EnderecoRepository;
 import com.udemy.spring.api.cursomc.repositories.EstadoRepository;
+import com.udemy.spring.api.cursomc.repositories.PagamentoRepository;
+import com.udemy.spring.api.cursomc.repositories.PedidoRepository;
 import com.udemy.spring.api.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -37,6 +45,10 @@ public class CursomcApplication implements CommandLineRunner { // CommandLineRun
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired 
+	private PagamentoRepository pagamentoRepository;;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -92,6 +104,25 @@ public class CursomcApplication implements CommandLineRunner { // CommandLineRun
 		
 		clienteRepository.save(cl1);
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cl1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cl1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cl1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		
+		
 	}
 
 }
---
+++ t
@@ -0,0 +1,59 @@
+[33mcommit bc1446397214d85fe282ba114dca8a27b00494ee[m[33m ([m[1;36mHEAD -> [m[1;32mdev[m[33m, [m[1;31morigin/dev[m[33m)[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Thu Feb 13 17:31:27 2020 -0300
+
+    ADD - Endpoint Get Clientes
+
+[33mcommit c6992640b4933a998f0663c035892de06e46b0da[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Thu Feb 13 17:11:55 2020 -0300
+
+    ADD - Domains Endereco,cliente, Telefones
+
+[33mcommit 87189f20784df706e19b83035c0053a10175a898[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Wed Feb 12 23:51:44 2020 -0300
+
+    ADD - Domains Estado e Cidade
+
+[33mcommit ece368a0b71e5a4a56590aee32ab6fdf6d8cf9cf[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Wed Feb 5 02:40:23 2020 -0300
+
+    ALT - Ajustes no endpoint de buscar categorias, ADD - ExcetionHandler Onjet Not FOund
+
+[33mcommit 662662a568b2fcbfff39605e4b3c98b9a9618aa9[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Wed Feb 5 01:45:50 2020 -0300
+
+    ADD - Domain Produtos e associação com Categoria
+
+[33mcommit b556da613de0ad4770b6d8231e955715f54ae95c[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Wed Feb 5 00:44:34 2020 -0300
+
+    ADD - Repository e Service para Categoria
+
+[33mcommit 2c7ec6f9b947a1ccba95712ee195b278b7a3879d[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Wed Feb 5 00:10:44 2020 -0300
+
+    ALT - Domain Categorias, ADD - Configuração H2
+
+[33mcommit c85cde02cf01783f5a2d2ee62b053387cedb1258[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Tue Feb 4 23:58:59 2020 -0300
+
+    ADD - Domain Categoria
+
+[33mcommit f1226cebd258a35ecdd306cf51efe836608173b7[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Tue Feb 4 23:36:42 2020 -0300
+
+    Testando Spring REST
+
+[33mcommit c47b9f9e3e812d38c501350472041ab39988a108[m[33m ([m[1;31morigin/master[m[33m, [m[1;32mmaster[m[33m)[m
+Author: Faris Ahmad <faris.s.ahmad@gmail.com>
+Date:   Tue Feb 4 23:23:21 2020 -0300
+
+    first commit - Projeto Criado
