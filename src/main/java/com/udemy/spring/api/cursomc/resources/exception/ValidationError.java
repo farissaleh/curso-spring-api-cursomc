6077f61f5e3bbe9d46605fe0ec9af47f55472031
Faris Ahmad <faris.s.ahmad@gmail.com>
2020-05-07 15:17:56 -0300

    PUT,DELETE,FIND PAGE do Cliente

---
+++ src/main/java/com/udemy/spring/api/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,24 @@
+package com.udemy.spring.api.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<FieldMessage>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+	//Nome no get q é convertido no json e não o nome no atributo
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+}
