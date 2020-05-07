1548959e12857a31cfd2a7f60222122007a216ec
Faris Ahmad <faris.s.ahmad@gmail.com>
2020-05-07 15:17:45 -0300

    PUT,DELETE,FIND PAGE do Cliente

--- src/main/java/com/udemy/spring/api/cursomc/services/ClienteService.java
+++ src/main/java/com/udemy/spring/api/cursomc/services/ClienteService.java
@@ -1,11 +1,18 @@
 package com.udemy.spring.api.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.udemy.spring.api.cursomc.domain.Cliente;
+import com.udemy.spring.api.cursomc.dto.ClienteDTO;
+import com.udemy.spring.api.cursomc.exceptions.DataIntegrityException;
 import com.udemy.spring.api.cursomc.exceptions.ObjectNotFoundException;
 import com.udemy.spring.api.cursomc.repositories.ClienteRepository;
 
@@ -17,8 +24,41 @@ public class ClienteService {
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> cliente = repo.findById(id);
-		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "+id //Supplier???
-				+ ", Tipo: "+ Cliente.class));
+		return cliente.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class));
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getNome());
 	}
 
 }
